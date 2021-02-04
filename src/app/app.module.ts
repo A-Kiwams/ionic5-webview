f59efbaebe6c20b3ffc815d9c7697d96cd3ae24c
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-02-04 10:37:01 -0800

    plugins
    First push

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomePage } from './home/home.page';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [
+    //AppComponent,
+    //HomePage
+  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    InAppBrowser,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController  } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(public navCtrl: NavController,private iab: InAppBrowser) {}
+  ngOnInit(){
+    const browser = this.iab.create('https://dealstartups.com/','_self',{location:'no'});
+
+  }
+
+}
